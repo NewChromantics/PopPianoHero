d33d6f41f510d0ac220e14e18069df4c3d82f380
Graham Reeves <graham@grahamreeves.com>
2019-07-15 23:47:46 +0100

    All keys!

--- PianoDuino/PianoDuino.ino
+++ PianoDuino/PianoDuino.ino
@@ -1,58 +1,74 @@
 #include "Keyboard.h"
 
+const int ButtonCount = 4;
+
 #if defined(ARDUINO_AVR_LEONARDO)
 	//	KeeYees Pro Micro ATmega32U4 5V 16MHz
 	//	gr: not sure if this is CH30 or not as it's already installed on this dev machine
-	const int ButtonStartPin = 2;
-	const int ButtonEndPin = 3;
+	const int AllButtonPowerPin = 2;
+	const int ButtonPins[ButtonCount] = { 3,4,5,6 };
 #else
 	#error Unconfigured board
 #endif
 
-bool PreviousPressed = false;
+const char ButtonKeys[ButtonCount] = {'1','2','3','4'};
+bool ButtonPressed[ButtonCount] = { false,false,false,false };
 
 
 void setup() 
 {
 	//	wait for leonardo serial to initialise
-	while ( !Serial )
+	//while ( !Serial )
 	{
 	}
 	Serial.begin(115200);
 	
-	pinMode( ButtonStartPin, OUTPUT );
-	pinMode( ButtonEndPin, INPUT );
-	
-	//  we set the internal-pullup value to high so when there is no connection, we
-	//  have the internal HIGH value reported
-	//  then turn the other end into essentially ground so when there IS a connection
-	//  the "ground" comes through.
-	//  therefore pressed=ground, unpressed=high
-	digitalWrite( ButtonEndPin, HIGH );
-	digitalWrite( ButtonStartPin, LOW );
+	pinMode( AllButtonPowerPin, OUTPUT );
+	digitalWrite( AllButtonPowerPin, HIGH );
+
+	for ( int b=0;	b<ButtonCount;	b++ )
+	{
+		pinMode( ButtonPins[b], INPUT );
+		digitalWrite( ButtonPins[b], LOW );
+	}
 
 	Serial.println("Starting keyboard...");
 	Keyboard.begin();
 }
 
-void loop() 
+bool ProcessButton(int ButtonIndex)
 {
-	bool NewPressed = digitalRead( ButtonEndPin ) == LOW;
-	if ( NewPressed == PreviousPressed )
-	{
-		delay(20);
-		return;
-	}
+	int ButtonPin = ButtonPins[ButtonIndex];
+	bool NewPressed = digitalRead( ButtonPin ) == HIGH;
+	bool& WasPressed = ButtonPressed[ButtonIndex];
+
+	if ( NewPressed == WasPressed )
+		return false;
 
 	if ( NewPressed )
 	{
-		Serial.println("Button Down...");
-		Keyboard.write('1');
-	}
-	else
-	{
-		Serial.println("Button up...");
+		Serial.print("Button ");
+		Serial.print(ButtonIndex);
+		Serial.println("Down...");
+	
+		Keyboard.write( ButtonKeys[ButtonIndex] );
 	}
 
-	PreviousPressed = NewPressed;
+	WasPressed = NewPressed;	
+	return true;
+}
+
+void loop() 
+{
+	bool AnyPressed = false;
+	AnyPressed |= ProcessButton(0);
+	AnyPressed |= ProcessButton(1);
+	AnyPressed |= ProcessButton(2);
+	AnyPressed |= ProcessButton(3);
+	
+	if ( !AnyPressed )
+	{
+		delay(10);
+		return;
+	}
 }
